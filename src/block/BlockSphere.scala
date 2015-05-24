0863b2b76f1933d2d484f8addc830a8f9443d5e4
Cobbleopolis <cobbleopolis@gmail.com>
2015-05-24 10:33:38 -0400

    Added s sphere block

--- src/block/BlockSphere.scala
+++ src/block/BlockSphere.scala
@@ -6,6 +6,6 @@ import registry.ModelRegistry
 class BlockSphere extends Block{
     blockID = BlockID.SPHERE
     gameModel = ModelRegistry.getModel("sphere")
-    texCoord = (2, 0)
+    texCoord = (6, 0)
     renderType = RenderTypes.NON_FULL_BLOCK
 }
